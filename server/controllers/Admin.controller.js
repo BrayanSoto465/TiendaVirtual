21d285a255b4ae05510cd143b028b15981c8a4b6
KamiloJ12 <camilo12052000@hotmail.com>
2021-09-07 18:33:31 -0500

    Modificar nombre y metodos

--- server/controllers/Admin.controller.js
+++ server/controllers/Admin.controller.js
@@ -3,38 +3,27 @@
 var Admin = require('../models/admin');
 var bcrypt = require('bcrypt-nodejs')
 
-const registro_admin = async function(req,res){
-    //
-    var data = req.body;
-    var admin_arr = [];
+const adminController = {}
 
-    admin_arr= await Admin.find({email:data.email});
+adminController.createAdmin = async (req, res) => {
+    const data = req.body;
+    const admin = await Admin.find({email: data.email});
 
-    if(admin_arr.length == 0){
-        /*  */
-
+    if(admin.length == 0){
         if(data.password){
-            bcrypt.hash(data.password,null,null, async function(err,hash){
+            bcrypt.hash(data.password, null, null, async function(err, hash) {
                 if(hash){
                     data.password = hash;
                     var reg = await Admin.create(data);
-                    res.status(200).send({data:reg});
-                }else{
-                    res.status(200).send({message:'ERROR',data:undefined});
-                }
-            })
+                    res.status(200).send({mensaje:reg});
+                }    
+            });
         }else{
-            res.status(200).send({message:'no hay una contraseña',data:undefined});
+            res.status(200).send({mensaje:'no hay contraseña',data: undefined});
         }
-
-        
     }else{
-        res.status(200).send({message:'El correo ya existe',data:undefined});
+        res.status(200).send({mensaje:'Admin ya existe',data: undefined});
     }
-
-    
 }
 
-module.exports = {
-    registro_admin
-}
+module.exports = adminController;
