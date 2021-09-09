53d718d7024d42968b60767f32378286de42853c
BrayanSoto465 <tronks_465@hotmail.com>
2021-09-09 07:26:28 -0500

    VALIDACION DE ADMIN

--- Admin/src/app/services/admin.service.ts
+++ Admin/src/app/services/admin.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
-
-import { GLOBAL } from './global'
+import { GLOBAL } from './GLOBAL';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +22,36 @@ export class AdminService {
 
   getToken(){
     return localStorage.getItem('token');
+  }
+
+  public isAuthenticated(allowRoles : string[]):boolean{
+  
+    const token = localStorage.getItem('token');
+        
+    if(!token){
+      return false;
+    }
+  
+    try {
+      const helper = new JwtHelperService();
+      var decodedToken = helper.decodeToken(token); 
+
+      console.log(decodedToken);
+
+      if(!decodedToken){
+        console.log('NO ACCCESO');
+        localStorage.removeItem('token');
+        return false;
+      } 
+
+    } catch (error) {
+      localStorage.removeItem('token');
+      return false;
+    }
+    
+
+    
+
+    return allowRoles.includes(decodedToken['role']);
   }
 }
